9dd63929de58764a49567401df76786a218c1ee5
shinahov <zzibra07@gmail.com>
2025-11-10 21:04:01 +0100

    add basic tokens

--- Tokenizer.py
+++ Tokenizer.py
@@ -8,3 +8,33 @@ class Token:
     def __repr__(self):
         return f"Token({self.type}, {self.value})"
 
+
+class Tokenizer:
+    KEYWORDS = {
+        "fn": "FN",
+        "create": "CREATE",
+        "set": "SET",
+        "if": "IF",
+        "else": "ELSE",
+        "loop": "LOOP",
+        "while": "WHILE",
+        "return": "RETURN",
+        "print": "PRINT",
+        "int": "TYPE_INT",
+        "class": "CLASS",
+        "end": "END",
+        "to": "TO",
+        "void": "TYPE_VOID",
+    }
+    TWO = {"->": "ARROW",
+           "=?": "EQUALSQ",
+           }
+
+    ONE = {
+        "=": "EQ", "+": "PLUS", "-": "MINUS", "*": "STAR", "/": "SLASH",
+        "<": "LT", ">": "GT",
+        "(": "LP", ")": "RP", ",": "COMMA", ";": "SEMI",
+        ":": "COLON", ".": "DOT", "[": "LBRK", "]": "RBRK",
+    }
+
+
